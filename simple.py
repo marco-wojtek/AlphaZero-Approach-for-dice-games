1e6c035595e614c34dffb3f7587d6d7bfc17d90d
Nullfeuer <marco.nullzwei@gmail.com>
2024-01-14 19:05:31 +0100

    add comments, fill Node-Methods, add MCTS class

--- simple.py
+++ simple.py
@@ -89,11 +89,20 @@ class Node:
         q_value = 1 - ((child.value_sum / child.visit_count) + 1) / 2
         return q_value + self.args['C'] * math.sqrt(math.log(self.visit_count) / child.visit_count)
     
+    #Two Optons on how to expand:
+    #1. append all possible children directly -> high cost
+    #2. append them "On-demand" -> good for high branching factor, but higher risk that parent
+    # will not be tested due to some bad first results
     def expand(self):
-        action = r.choice(self.expandable_moves)
-        self.expandable_moves[action] = -1
-
+        #if the action taken was 0 the node has to append all possible rethrow options
+        # if self.action_taken == 0:
+        #     for permutation in range(1,len(all_permutations)):
+        #         child_state = copy.deepcopy(self.state)
+        #         child_state[0] = rethrow(child_state[0],all_permutations[permutation])
+        #         self.children[''.join(str(x) for x in all_permutations[permutation])] = Node(self.game,self.args,child_state,self.active_player,self,None,True)
         #append all possible dice outcomes to the current node -> maybe need reposition
+        #for chance nodes I need to regard states where e.g. only one dice is rethrown since it means
+        #that the possible dice states change| (6,6,6) cannot be thrown if only the first die of (1,2,3) is rethrown
         # if self.ischance:
         #     for dices in sorted_possible_dice_states:
         #         child_state = copy.deepcopy(self.state)
@@ -104,24 +113,65 @@ class Node:
     
     def simulate(self):
         points, is_terminal = self.game.get_points_and_terminated(self.state)
-        value = np.argmax(points)
+        value = np.argmax(points) if np.count_nonzero(points==np.max(points))==1 else -1
         if is_terminal:
             return value
         
         rollout_state = copy.deepcopy(self.state)
-        #TODO
-        return
+        player = self.active_player
+        while not is_terminal:
+            v = self.game.get_valid_moves(rollout_state,player,0)
+            act = r.choice(v)
+            rollout_state = self.game.get_next_state(rollout_state,player,act)
+            rollout_state = self.game.get_next_state(rollout_state,player,0,[1,1,1,1,1])
+            player = (player+1)%len(self.state[1])
+            points, is_terminal = self.game.get_points_and_terminated(self.state)
+        return np.argmax(points) if np.count_nonzero(points==np.max(points))==1 else -1
     
     def backpropagate(self,value):
-        self.value_sum += value
+        #2-player simulation returns either 0 or 1 for the corresponding winner
+        #a tie returns -1 meaning no winner; MCTS shouldn't strive for a tie
+        self.value_sum += -1**(value!=self.active_player)
         self.visit_count += 1
-        
-        # value = 
+         
         if self.parent is not None:
             self.parent.backpropagate(value) 
 
+
+class MCTS:
+    def __inti__(self, game, args):
+        self.game = game
+        self.args = args
+    
+    #returns the probabilities for the possible actions
+    def search(self,state,player):
+        root = Node(self.game,self.args,state,player)
+
+        for search in range(self.args['num_searches']):
+            node = root
+
+            while node.is_fully_expanded():
+                node = node.select()
+            
+            points, is_terminal = self.game.get_points_and_terminated(node.state)
+            value = np.argmax(points) if np.count_nonzero(points==np.max(points))==1 else -1
+
+            if not is_terminal:
+                node = node.expand()
+                value = node.simulate()
+            
+            node.backpropagate(value)
+        
+        action_probs = np.zeros(options+1)
+        for child in root.children:
+            action_probs[child.action_taken] += child.visit_count
+        action_probs /= np.sum(action_probs)
+        return action_probs
+
+
 possible_dice_states = list(iter.product(range(1,7),repeat=3)) #len =  216
 sorted_possible_dice_states = list(iter.combinations_with_replacement(range(1,7),r=3)) #len = 56
+all_permutations = list(iter.product(range(0,2),repeat=3))
 #contains the probability distribution for sorted dice events to happen
 dice_state_probabilities = {}
 for d_state in possible_dice_states:
@@ -148,3 +198,7 @@ print(dice_state_probabilities)
 outcome = r.choices(list(dice_state_probabilities.keys()),weights=list(dice_state_probabilities.values()))
 print(outcome)
 
+Yahtzee = yahtzee(2)
+state = Yahtzee.get_initial_state()
+state[0] = rethrow(state[0],all_permutations[1])
+print(state)
