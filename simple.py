d20abb2cf273e4363dce8c1b0904dd4a5934bbc3
Nullfeuer <marco.nullzwei@gmail.com>
2024-01-15 19:06:53 +0100

    add expand function/ work on all functions; first Bug fixes for functions

--- simple.py
+++ simple.py
@@ -52,7 +52,8 @@ class yahtzee:
         return np.sum(state[1]>0,axis=1), not np.any(state[1]==-1)
 
 class Node:
-    def __init__(self, game, args, state, player, parent=None, action_taken=None, ischance = False):
+    #initialises a node with game, gamestate, the latest active player, parent node, last action taken, wether the node is chance or descision and rethrow choice which is only relevant for last action 0
+    def __init__(self, game, args, state, active_player, parent=None, action_taken=None, ischance = False, rethrow_choice =  None):
         self.game = game
         self.args = args
         self.state = state
@@ -60,18 +61,23 @@ class Node:
         self.action_taken = action_taken
         self.ischance = ischance
         self.children = {}
-        self.expandable_moves = game.get_valid_moves(state)
-        self.active_player = player
+        self.active_player = active_player
+        self.throw = 0 if (self.parent is None or self.parent.active_player!=self.active_player) else self.parent.throw + (self.action_taken==0)
+        #one list of moves/or rethrow choices for descision nodes| a list of possible dice states plus their probobilities
+        
+        self.expandable_moves = calc_sorted_possible_dice_states(self.state[0],rethrow_choice) if action_taken is None else all_permutations if  action_taken==0 else game.get_valid_moves(self.state,self.active_player,self.throw)
+        
         
         self.visit_count = 0
         self.value_sum = 0
 
     def is_fully_expanded(self):
-        return len(self.expandable_moves) == 0 and len(self.children) > 0
+        return len(self.expandable_moves) == len(self.children) and len(self.children) > 0
     
     def select(self):
-        if self.ischance:
-            outcome = r.choices(list(dice_state_probabilities.keys()),list(dice_state_probabilities.values()))
+        if self.ischance:#for chance nodes expandle_moves[1] contains the probability distribution 
+            dsp = self.expandable_moves[1]
+            outcome = r.choices(list(dsp.keys()),list(dsp.values()))
             return self.children[outcome]
         
         best_child = None
@@ -94,22 +100,38 @@ class Node:
     #2. append them "On-demand" -> good for high branching factor, but higher risk that parent
     # will not be tested due to some bad first results
     def expand(self):
-        #if the action taken was 0 the node has to append all possible rethrow options
-        # if self.action_taken == 0:
-        #     for permutation in range(1,len(all_permutations)):
-        #         child_state = copy.deepcopy(self.state)
-        #         child_state[0] = rethrow(child_state[0],all_permutations[permutation])
-        #         self.children[''.join(str(x) for x in all_permutations[permutation])] = Node(self.game,self.args,child_state,self.active_player,self,None,True)
-        #append all possible dice outcomes to the current node -> maybe need reposition
-        #for chance nodes I need to regard states where e.g. only one dice is rethrown since it means
-        #that the possible dice states change| (6,6,6) cannot be thrown if only the first die of (1,2,3) is rethrown
-        # if self.ischance:
-        #     for dices in sorted_possible_dice_states:
-        #         child_state = copy.deepcopy(self.state)
-        #         child_state[0] = np.asarray(dices)
-        #         self.children[''.join(str(x) for x in dices)] = Node(self.game,self.args,child_state,self.active_player,self,None,False)
-        #TODO
-        return
+        
+        #V2. append on-demand except for chance nodes
+        #case if last decision node chose "rethrow" ~ action 0 following node is chance node
+        if self.action_taken == 0:
+            index = r.choice(np.ravel(np.argwhere([self.expandable_moves[i]!=0 for i in range(len(self.expandable_moves))])))
+            permutation = self.expandable_moves[index]
+            child_state = copy.deepcopy(self.state)
+            child = Node(self.game,self.args,child_state,self.active_player,self,None,True)
+            self.children[''.join(str(x) for x in permutation)] = child
+            self.expandable_moves[index] = 0
+
+        #append all chance outcomes following node is decision node
+        elif self.ischance:
+            for dices in self.expandable_moves[0]:
+                print(dices)
+                child_state = copy.deepcopy(self.state)
+                child_state[0] = np.asarray(dices)
+                child = Node(self.game,self.args,child_state,self.active_player,self,None,False)
+                self.children[''.join(str(x) for x in dices)] = child
+
+        #normal decision node case following node is either chance node or decision node for rethrow
+        elif self.action_taken is None:
+            action = self.expandable_moves[r.choice(np.where(self.expandable_moves!=-1))]
+            child_state = copy.deepcopy(self.state)
+            if action == 0:
+                child = Node(self.game,self.args,child_state,self.active_player,self,action,False)
+            else:
+                # child_state[0] = rethrow(child_state[0],np.ones(len(child_state[0]))) # rethrow isnt happening actively
+                child = Node(self.game,self.args,child_state,(self.active_player+1)%len(child_state[1]),self,action,True)
+            self.children[action] = child
+            self.expandable_moves[action]  = -1
+        return child
     
     def simulate(self):
         points, is_terminal = self.game.get_points_and_terminated(self.state)
@@ -139,7 +161,7 @@ class Node:
 
 
 class MCTS:
-    def __inti__(self, game, args):
+    def __init__(self, game, args):
         self.game = game
         self.args = args
     
@@ -171,34 +193,42 @@ class MCTS:
 
 possible_dice_states = list(iter.product(range(1,7),repeat=3)) #len =  216
 sorted_possible_dice_states = list(iter.combinations_with_replacement(range(1,7),r=3)) #len = 56
-all_permutations = list(iter.product(range(0,2),repeat=3))
-#contains the probability distribution for sorted dice events to happen
-dice_state_probabilities = {}
-for d_state in possible_dice_states:
-    sorted_d_state = np.sort(d_state)
-    index = ''.join(str(x) for x in np.sort(sorted_d_state))
-    if index not in dice_state_probabilities:
-        dice_state_probabilities[index] = 0
-    dice_state_probabilities[index] += 1
-for d in dice_state_probabilities:
-    dice_state_probabilities[d] = dice_state_probabilities[d]/len(possible_dice_states)
+# def sorted_possible_dice_states(last_dice,rethrows):
+#     return sorted_possible_dice_states
+all_permutations = list(iter.product(range(0,2),repeat=3))[1:] #rethrow option (0,0,0) is filtered out
 
-Yahtzee = yahtzee(2)
-state = Yahtzee.get_initial_state()
-child = {}
-i = 0
-for dices in sorted_possible_dice_states:
-    child_state = copy.deepcopy(state)
-    child[''.join(str(x) for x in dices)] = state
-    i +=1
-    if i==3:
-        break
+#calculates the probability distribution of a given number of dice_states; dice_states are handled in their sorted form
+def calc_dice_state_probabilities(all_possible_dice_states):
+    dice_state_probabilities = {}
+    for d_state in all_possible_dice_states:
+        sorted_d_state = np.sort(d_state)
+        index = ''.join(str(x) for x in np.sort(sorted_d_state))
+        if index not in dice_state_probabilities:
+            dice_state_probabilities[index] = 0
+        dice_state_probabilities[index] += 1
+    for d in dice_state_probabilities:
+        dice_state_probabilities[d] = dice_state_probabilities[d]/len(all_possible_dice_states)
+    return dice_state_probabilities
 
-print(dice_state_probabilities)
-outcome = r.choices(list(dice_state_probabilities.keys()),weights=list(dice_state_probabilities.values()))
-print(outcome)
+#returns a list of poossible dice states if a specific number of dices are changed only
+def calc_sorted_possible_dice_states(current_dice, changing_dice):
+    if changing_dice is None or np.all(changing_dice):
+        return sorted_possible_dice_states, calc_dice_state_probabilities(possible_dice_states)
+    
+    sorted_states = list()
+    state_list = list()
+    for elem in possible_dice_states:
+        is_similar = True
+        for i in range(len(current_dice)):
+            if not changing_dice[i] and elem.count(current_dice[i])<np.count_nonzero(np.ma.masked_array(current_dice,mask=changing_dice)==current_dice[i]):
+                is_similar = False
+                break
 
-Yahtzee = yahtzee(2)
-state = Yahtzee.get_initial_state()
-state[0] = rethrow(state[0],all_permutations[1])
-print(state)
+        if is_similar:
+            state_list.append(elem)
+            if tuple(sorted(elem)) not in sorted_states:
+                sorted_states.append(tuple(sorted(elem))) 
+
+    return sorted_states,calc_dice_state_probabilities(state_list)
+
+
